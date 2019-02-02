b5ab1b3e808369d527e4c6bfe3cf71978ee84b5f
Carlotta Segna <carlotta@MBP-di-Carlotta.homenet.telecomitalia.it>
2019-02-02 18:10:20 +0100

    close #68, #69, #70, #71, #73, #74, #75

--- Documentazione/RR/Utility/script/istogramma/main.py
+++ Documentazione/RR/Utility/script/istogramma/main.py
@@ -1,18 +1,18 @@
 import plot_generator as pg
 
-plot_title = 'Totale ore Rendicontate'   # il grafo si salva con questo nome
+plot_title = 'Totale ore con Investimento'   # il grafo si salva con questo nome
 y_label = 'ore'
 x_values = ['Marco Chilese', 'Marco Favaro', 'Diego Mazzalovo',
             'Carlotta Segna', 'Matteo Slanzi', 'Bogdan Stanciu', 'Luca Violato']
 colors = ["#9DA1AA", "#C51D34", "#415A66", "#7ECCFD", "#57A639", "#F5D033"] 
 
 d = {
-    'Responsabile': [10,12,10,0,10,5,13],
-    'Amministratore': [13,10,11,17,0,10,5],
-    'Analista': [3,18,5,15,5,23,10],
-    'Progettista': [32,14,33,10,32,25,19],
-    'Programmatore': [16,19,34,46,32,12,31],
-    'Verificatore': [31,32,12,17,26,30,27]
+    'Responsabile': [8,8,8,13,8,20,8],
+    'Amministratore': [13,10,11,17,10,10,20],
+    'Analista': [13,36,23,15,13,33,20],
+    'Progettista': [32,14,35,10,31,25,22],
+    'Programmatore': [16,19,34,46,35,12,33],
+    'Verificatore': [48,43,19,29,33,30,27]
 }
 
 width = 0.70       # the width of the bars: can also be len(x) sequence
